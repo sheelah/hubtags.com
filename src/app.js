373d0595c489088883f0dcf3bc9e735846e32133
Sheelah Brennan <slb53@hotmail.com>
2016-01-13 13:10:32 -0700

    Add Sass loader and use Sass instead of CSS; add es5 app.js file to show difference vs ES6

--- src/app.js
+++ src/app.js
@@ -1,2 +1,10 @@
-document.write('Hello Frontend Masters!')
+import React from 'react';
+import styles from './sass/main.scss';
 
+const Hello = React.createClass({
+  render() {
+    return <div><h1>Hello, {this.props.name}!</h1></div>
+  }
+});
+
+React.render(<Hello name="sheelah" />, document.body);
