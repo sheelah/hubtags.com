f56601fd3185abfb5c2fab6abdedbfb24a93fd87
Sheelah Brennan <slb53@hotmail.com>
2016-01-14 00:01:47 -0700

    Add layout component and render all other pages using it as a wrapper

--- src/router.js
+++ src/router.js
@@ -3,8 +3,20 @@ import Router from 'ampersand-router';
 import styles from './styles/main.styl';
 import ReposPage from './pages/repos';
 import PublicPage from './pages/public';
+import Layout from './layout';
 
 export default Router.extend({
+  renderPage(page, opts={layout: true}) {
+    if (opts.layout) {
+      page = (
+        <Layout>
+          {page}
+        </Layout>
+      )
+    };
+    React.render(page, document.body)
+  },
+
   routes: {
     '': 'public',
     'repos': 'repos'
@@ -12,12 +24,12 @@ export default Router.extend({
 
   // Route handlers
   public() {
-    React.render(<PublicPage/>, document.body);
+    this.renderPage(<PublicPage/>, {layout: false} );
 
   },
 
   repos() {
-    React.render(<ReposPage/>, document.body);
+    this.renderPage(<ReposPage/>);
 
   }
 });
