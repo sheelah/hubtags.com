d4a9c6b66f95ccaf1f1370cd174933d1c3295e4a
Sheelah Brennan <slb53@hotmail.com>
2016-01-16 13:12:51 -0700

    Add delete click event and needed idAttribute to allow for deletion

--- src/components/label-item.js
+++ src/components/label-item.js
@@ -16,6 +16,13 @@ export default React.createClass({
     this.props.label.editing = false;
   },
 
+  onDeleteClick(event) {
+    event.preventDefault();
+    // Set wait:true on destroy to keep in UI until actual deletion
+    // from the collection
+    this.props.label.destroy();
+  },
+
   render() {
     const {label} = this.props;
     const cssColor = '#' + label.color;
@@ -39,7 +46,7 @@ export default React.createClass({
           <span className='label-color' style={{backgroundColor: cssColor}}>&nbsp;</span>
           <span>{label.name}</span>
           <span onClick={this.onEditClick} className='octicon octicon-pencil'></span>
-          <span className='octicon octicon-x'></span>
+          <span onClick={this.onDeleteClick} className='octicon octicon-x'></span>
         </div>
       );
     }
--- src/models/label.js
+++ src/models/label.js
@@ -1,6 +1,10 @@
 import Model from 'ampersand-model';
+import githubMixin from '../helpers/github-mixin';
 
-export default Model.extend({
+export default Model.extend(githubMixin, {
+  // Set ID so we can easily delete labels
+  idAttribute: 'name',
+
   props: {
     name: 'string',
     color: 'string'
