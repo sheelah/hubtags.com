586e5f97f088bc0d08bdebc46b2e96f8d35a2816
Sheelah Brennan <slb53@hotmail.com>
2016-01-18 18:36:13 -0700

    update prod hostname

--- src/config.js
+++ src/config.js
@@ -5,7 +5,7 @@ const config = {
       clientId: '8df2128b00fcc1dc1445'
   },
 
-  'labelr.surge.sh': {
+  'cinnamon.surge.sh': {
       authUrl: 'https://secure-crag-7197.herokuapp.com/authenticate',
       clientId: 'a5361c10ef0106e96efc'
   }
