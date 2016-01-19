5198f1b9c4288a53cbc3d4e4f9621d780cb4f79f
Sheelah Brennan <slb53@hotmail.com>
2016-01-18 19:09:47 -0700

    make top nav server-rendered also for best UX

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ require('babel/register');
 var React = require('react');
 var getConfig = require('hjs-webpack')
 var PublicPage = require('./src/pages/public');
+var Layout = require('./src/layout');
 
 module.exports = getConfig({
   in: 'src/app.js',
@@ -9,11 +10,12 @@ module.exports = getConfig({
   clearBeforeBuild: true,
   html: function(context) {
     const publicPage = React.renderToString(React.createElement(PublicPage));
+    const layoutPage = React.renderToString(React.createElement(Layout, {me: {}}));
 
     // Set up 200.html file to serve as default page
     return {
       'index.html': context.defaultTemplate({html: publicPage}),
-      '200.html': context.defaultTemplate()
+      '200.html': context.defaultTemplate({html:layoutPage})
     }
   }
 })
